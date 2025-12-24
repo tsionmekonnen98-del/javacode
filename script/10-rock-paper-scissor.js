d2960aff0094c0c50470b36e5d656f0cfe383356
tsion <tsionmekonnen98@gmail.com>
2025-12-24 03:06:33 -0800

    commit

---
+++ script/10-rock-paper-scissor.js
@@ -0,0 +1,86 @@
+let score = JSON.parse(localStorage.getItem('score')) ||{
+      wins: 0,
+      losses: 0,
+      ties: 0
+   };
+   
+   updateScoreElement();
+
+
+function playGame(playerMove){
+
+   pickComputerMove();
+let result = '';
+
+  if (playerMove === 'scissors'){
+   if (computerMove === 'rock') {
+    result = 'You lose.';
+   } else if (computerMove === 'paper') {
+    result = 'You win.';
+   } else if (computerMove === 'scissors') {
+      result = 'Tie.';
+   }
+
+
+  }else if(playerMove === 'paper'){
+     if(computerMove === 'rock'){
+    result = 'You win.';
+   } else if (computerMove === 'paper'){
+    result = 'Tie.';
+   } else if (computerMove === 'scissors'){
+      result = 'You lose.';
+   }
+  }
+   else if(playerMove === 'rock'){
+    if(computerMove === 'rock'){
+    result = 'You win.';
+   } else if (computerMove === 'paper'){
+    result = 'Tie.';
+   } else if (computerMove === 'scissors'){
+      result = 'You lose.';
+   }
+  }
+
+   if(result === 'You win.'){
+
+      score.wins += 1;
+   } else if(result === 'You lose.'){
+      score.losses += 1;
+   }else if(result === 'Tie.'){
+      score.ties +=1;
+   }   
+
+   localStorage.setItem('score', JSON.stringify(score));
+     
+     updateScoreElement();
+     
+     document.querySelector('.js-result').innerHTML = result;
+
+     document.querySelector('.js-moves').innerHTML =  `You
+      <img src="images/${playerMove}-emoji.png" class="move-icon" >
+      <img src="images/${computerMove}-emoji.png" class="move-icon" >
+      Computer`
+       
+     document.querySelector('js-result').innerHTML = '';
+
+  
+    }
+
+    function updateScoreElement(){
+       document.querySelector('.js-score').innerHTML = `wins: ${score.wins}, losses: ${score.losses}, Ties: ${score.ties}`;
+  
+    }
+
+    let computerMove='';
+    function pickComputerMove(){
+ const randomNumber = Math.random();/*math.random()is used to display random no. between 0 and 1.*/
+  
+  if(randomNumber >= 0 && randomNumber < 1/3){
+    computerMove = 'rock';
+  } else if(randomNumber >= 1/3 && randomNumber < 2/3){
+    computerMove ='paper';
+  }  else if (randomNumber >= 2/3 && randomNumber <1){
+    computerMove = 'scissors';
+    
+  }
+}
