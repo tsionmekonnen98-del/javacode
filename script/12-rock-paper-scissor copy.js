5aaba13c5467f905119059987e5841de34ff63c2
tsion <tsionmekonnen98@gmail.com>
2026-02-24 10:59:29 -0800

    comit

--- script/12-rock-paper-scissor copy.js
+++ script/12-rock-paper-scissor copy.js
@@ -22,7 +22,19 @@ let score = JSON.parse(localStorage.getItem('score')) ||{
      }
      
   }
+       document.querySelector('.js-rock-button').addEventListener('click', ()=>{
+         playGame('rock');
+       });
 
+       
+       document.querySelector('.js-paper-button').addEventListener('click', ()=>{
+         playGame('paper');
+       });
+
+       
+       document.querySelector('.js-scissor-button').addEventListener('click', ()=>{
+         playGame('scissor');
+       });
 
 function playGame(playerMove){
 
